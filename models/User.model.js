99182d5cbcfd4ec51611782be089a9d2a6da4093
marcelasalvador <salvadormarcela18@gmail.com>
2022-11-29 16:57:48 -0600

    auth updates

---
+++ models/User.model.js
@@ -0,0 +1,15 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema(
+    {
+      email: String,
+      password: String,
+    },
+    {
+      timestamps: true
+    }
+  );
+
+const User = model("User", userSchema);
+
+module.exports = User;
---
+++ routes/auth.js
@@ -0,0 +1,86 @@
+const express = require('express')
+const router = express.Router();
+
+const User = require('../models/User.model')
+
+const bcryptjs = require('bcryptjs')
+
+const saltRounds = 10
+
+
+router.get('/login', (req, res, next) => {
+    res.render('auth-views/login')
+})
+
+router.post('/login', (req, res, next) => {
+
+    if (!req.body.email|| !req.body.password) {
+        res.render('auth-views/login', {message : "Both fields are required"})
+        return;
+    } 
+    
+    User.findOne({email: req.body.email})
+    .then((foundUser) => {
+        if (!foundUser) {
+            res.render('auth-views/login', {message: "This User does not exist"})
+        } else {
+            let correctPassword = bcryptjs.compareSync(req.body.password, foundUser.password);
+            if(correctPassword) {
+                req.session.user = foundUser;
+                res.render('index', {message: "You have logged in"})
+            } else {
+                res.render('auth-views/login', {message: "Incorrect Password or Email"})
+            }
+        }
+    })    
+})
+
+router.get('/signup', (req, res, next) => {
+    res.render('auth-views/signup')
+})
+
+
+router.post('/signup', (req, res, next) => {
+
+    if (!req.body.fullName || !req.body.email || !req.body.password)
+    {
+        res.render('auth-views/signup', {message: "You must fill out all fields"})
+        return;
+    }
+
+    const salt = bcryptjs.genSaltSync(saltRounds)
+    const hashedPass = bcryptjs.hashSync(req.body.password, salt)
+
+
+    User.findOne({email: req.body.email})
+        .then((foundUser) => {
+            if (foundUser){
+            res.render('auth-views/signup', {message: "You have already signed up"})
+            return
+        } else {
+                User.create({
+                    fullName: req.body.fullName,
+                    email: req.body.email,
+                    password: hashedPass
+                })
+                .then(() => {
+                    res.redirect('/auth/login')
+                })
+                .catch((err) => {
+                    console.log(err)
+                })
+            }
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+
+})
+
+
+router.get('/logout', (req, res, next) => {
+    req.session.destroy()
+    res.render('auth-views/login', {message: "You have logged out"})
+})
+
+module.exports = router
